ba6bb8e3353bfb2de1772d0ee42a2466493823ae
Shawn Kim <sjk002@ucsd.edu>
2022-05-13 17:23:00 -0700

    Structure

---
+++ src/components/navbar/index.js
@@ -0,0 +1,14 @@
+import './style.scss';
+import '../../styles/base.scss'
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <h1>hello</h1>
+    </div>
+  )
+}
+
+export default Navbar
