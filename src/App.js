2dcd740b8c1aaf961c6e51ecacec0f992cafaecf
Tracey <traceydu0122@gmail.com>
2022-05-19 11:30:10 -0700

    Why Apply page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import FAQ from "./components/faq";
+import WhyApply from "./components/whyapply";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <WhyApply />
       <FAQ />
       <Footer />
     </div>
