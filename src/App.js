d4ff1a2cfc5f1057f16888cbd165ef46551db640
Shawn Kim <sjk002@ucsd.edu>
2022-05-22 14:21:15 -0700

    navbar updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/main.scss';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import FAQ from "./components/faq";
