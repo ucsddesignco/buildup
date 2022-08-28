11d51d9e2d0f1a2bfa0e1308b6b8f82fa05c0725
brianliu1412 <briangliu1@gmail.com>
2022-08-28 16:14:52 -0700

    Final revisions to build up articles-modal

--- src/components/articles/girls_rising/index.js
+++ src/components/articles/girls_rising/index.js
@@ -41,105 +41,122 @@ const GirlsRising = () => {
           </div>
         </div>
         <div className="group">
-          <img src={group_duo} className="groupduo"></img>
-        </div>
+          <div className="photo"></div>
+        </div> 
       </div>
-      <div className="page2">
-        {/*<img src={mobile_divider} className="divider"> </img>*/}
-        <h3 className="team-title">THE TEAM</h3>
-        <div className="builders-div">
-          <Grid container spacing={6} className="builders-grid">
-            <Grid item className="grid-item" xs={6} md={4}>
-              <div className="builder-img">
-                <img src={karen}></img>
-              </div>
-              <div className="tags">
-               <p>{"KAREN NIETO"}</p>
-               <p>UX Researcher</p>
-              </div>
-            </Grid>
-            <Grid item className="grid-item" xs={6} md={4}>
-              <div className="builder-img">
-                <img src={esther}></img>
-              </div>
-              <div className="tags">
-                <p>{"ESTHER LIU"}</p>
-                <p>UX Designer</p>
-              </div>
-            </Grid>
-            <Grid item className="grid-item" xs={6} md={4}>
-              <div className="builder-img">
-                <img src={hanyi}></img>
-              </div>
-              <div className="tags">
-                <p>{'HANYI WANG'}</p>
-                <p>Visual Designer</p>
-              </div>
-            </Grid>
-            <Grid item className="grid-item" xs={6} md={4}>
-              <div className="builder-img">
-                <img src={wanting}></img>
-              </div>
-              <div className="tags">
-                <p>{"WANTING (ASH) ZHOU"}</p>
-                <p className="text">Visual Designer+ Marketing Stratgeist</p>
-              </div>
-            </Grid>
-            <Grid item className="grid-item" xs={6} md={4}>
-              <div className="builder-img">
-                <img src={don}></img>
-              </div>
-                <div className="tags">
-                  <p>{"DON LE"}</p>
-                  <p>Web Builder</p>
-                </div>
-            </Grid>
-            <Grid item className="grid-item" xs={6} md={4}>
-              <div className="builder-img">
-                <img src={jessica}></img>
-              </div>
-                <div className="tags">
-                  <p>{"JESSICA LIU"}</p>
-                  <p>Web Builder</p>
-                </div>
-            </Grid>
-          </Grid>
+      <div className="mobile-divider">
+          <img src={mobile_divider}></img>
         </div>
-        <div className="shapes">
+        <div className="web-divider">
+          <img src={web_divider}></img>
+        </div>
+      <div className="page2">
+        <h3 className="team-title">THE TEAM</h3>
+        <div className="mentors-div">
+            <Grid direction="row" 
+              container spacing= {2} 
+              className="mentors-grid"
+              alignItems="center"
+              justifyContent="space-evenly">
+              <Grid item className="mentor-grid-item" xs={6} md={4}>
+                <div className="mentor-img">
+                  <img src={karen}></img>
+                </div>
+                <div className="tags">
+                  <p>{"{KAREN NIETO}"}</p>
+                  <p>UX Researcher</p>
+                </div>
+              </Grid>
+              <Grid item className="mentor-grid-item" xs={6} md={4}>
+                <div className="mentor-img">
+                  <img src={esther}></img>
+                </div>
+                <div className="tags">
+                  <p>{"{ESTHER LIU}"}</p>
+                  <p>UX Designer</p>
+                </div>
+              </Grid>
+              <Grid item className="mentor-grid-item" xs={6} md={4}>
+                <div className="mentor-img">
+                  <img src={hanyi}></img>
+                </div>
+                <div className="tags">
+                  <p>{"{HANYI WANG}"}</p>
+                  <p>Visual Designer
+                  </p>
+                </div>
+              </Grid>
+              <Grid item className="mentor-grid-item" xs={6} md={4}>
+                <div className="mentor-img">
+                  <img src={wanting}></img>
+                </div>
+                <div className="tags">
+                  <p>{"{WANTING ASH ZHOU}"}</p>
+                  <p className="wanting">Visual Designer + Marketing Stratgeist</p>
+                </div>
+              </Grid>
+              <Grid item className="mentor-grid-item" xs={6} md={4}>
+                <div className="mentor-img">
+                  <img src={don}></img>
+                </div>
+                <div className="tags">
+                  <p>{"{DON LE}"}</p>
+                  <p>Web Builder</p>
+                </div>
+              </Grid>
+              <Grid item className="mentor-grid-item" xs={6} md={4}>
+                <div className="mentor-img">
+                  <img src={jessica}></img>
+                </div>
+                <div className="tags">
+                  <p>{"{JESSICA LIU}"}</p>
+                  <p>Web Builder</p>
+                </div>
+              </Grid>
+            </Grid>
+          </div>
+        <div className="mobile-shapes">
           <img src={mobile_shapes} className="shape"></img>
         </div>
+        <div className="web-shapes">
+          <img src={web_shapes} className="shape"></img>
+        </div>
+        </div>
         <div className="page3">
-          <p className="mentors-header">THE MENTORS</p>
+          <h3 className="mentors-header">THE MENTORS</h3>
           <div className="mentors-div">
-            <Grid container-spacing={6} direction="row" className="mentors-grid">
-              <Grid item className="grid-item" xs={6} md={4}>
+            <Grid container spacing={2} direction="row" className="mentors-grid">
+              <Grid item className="mentor-grid-item" xs={6} md={4}>
                 <div className="mentor-img">
                   <img src={shaira}></img>
                 </div>
                 <div className="tags">
-                  <p>{"SHAIRA TUAZON"}</p>
+                  <p>{"{SHAIRA TUAZON}"}</p>
                   <p>Product Designer, Meta</p>
                 </div>
               </Grid>
-              <Grid item className="grid-item" xs={6} md={4}>
+              <Grid item className="mentor-grid-item" xs={6} md={4}>
                 <div className="mentor-img">
                   <img src={heidi}></img>
                 </div>
-                <p>{"HEIDI HERNANDEZ"}</p>
-                <p>UX Designer, Google</p>
+                <div className="tags">
+                  <p>{"{HEIDI HERNANDEZ}"}</p>
+                  <p>UX Designer, Google</p>
+                </div>
               </Grid>
-              <Grid item className="grid-item" xs={6} md={4}>
+              <Grid item className="mentor-grid-item" xs={6} md={4}>
                 <div className="mentor-img">
                   <img src={bart}></img>
                 </div>
-                <p>{"BART VAN DE VEL"}</p>
-                <p>Global Program Manager, Meta</p>
+                <div className="tags">
+                  <p>{"{BART VAN DE VEL}"}</p>
+                  <p>Global Program Manager, Meta</p>
+                </div>
               </Grid>
             </Grid>
           </div>
         </div>
       </div> 
-    </div>
   
   );
 };
