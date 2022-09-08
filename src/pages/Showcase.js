a82042d11742f6c6314c734acdf4bb27dfc0cbf2
Shawn Kim <sjk002@ucsd.edu>
2022-09-08 14:33:42 -0700

    loading animation

--- src/pages/Showcase.js
+++ src/pages/Showcase.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../components/footer";
 import NewModal from "../components/newModal";
 import ShowcaseArticles from "../components/showcaseArticles";
@@ -6,16 +6,50 @@ import ShowcaseLanding from "../components/showcaseLanding";
 import ShowcaseNavbar from "../components/showcaseNavbar";
 import ShowcaseShowcase from "../components/showcaseShowcase";
 import ShowcaseThanks from "../components/showcaseThanks";
+import '../styles/main.scss';
+import gsap from 'gsap';
+
+import Opening from "../assets/images/Showcase/opening.mov";
+import Opening2 from "../assets/images/Showcase/opening.mp4";
+import Overlay from "../assets/images/Showcase/shop-overlay.mp4";
+import NewOverlay from "../assets/images/Showcase/newopening.mp4";
+
 const Showcase = () => {
+
+  useEffect(() => {
+    const tl1 = gsap.timeline();
+    tl1
+      .to('#animation-container', {
+        y: '-100vh',
+        duration: 0.75,
+        delay: 2.5,
+      })
+
+      const tl2 = gsap.timeline();
+      tl2
+        .to('#delay', {
+          opacity: 1,
+          delay: 2.5,
+        })
+  }, [])
+
+  
+  
   return (
     <>
-      <ShowcaseNavbar currentPage={"/"} />
-      <ShowcaseLanding />
-      <ShowcaseShowcase />
-      <ShowcaseArticles />
-      <ShowcaseThanks />
-      <NewModal />
-      <Footer />
+      <video className="showcase-opening" id="animation-container" muted loop autoPlay >
+        <source src={NewOverlay} type="video/mp4" />
+      </video>
+
+      <div id="delay">
+        <ShowcaseNavbar currentPage={"/"} />
+        <ShowcaseLanding />
+        <ShowcaseShowcase />
+        <ShowcaseArticles />
+        <ShowcaseThanks />
+        <NewModal />
+        <Footer />
+      </div>
     </>
   );
 };
